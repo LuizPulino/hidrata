53f6d9a2ebf0741fa5bec4583970a97669f79f65
Luiz Pulino <luizpulino@gmail.com>
2024-09-24 17:49:21 -0300

    manifest

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 import './modules/router.js';
-$("h1").text("Defender");
-
-window.route = route;
+$("main").load("pages/start.html");
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('./service-worker.js')
+        .then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch(function(err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+}
