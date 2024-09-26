519fd5881a42e209cdddb6e4602c163810bdef2a
Luiz Pulino <luizpulino@gmail.com>
2024-09-26 16:45:39 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,12 +1,2 @@
 import './modules/router.js';
 $("main").load("pages/home.html");
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('./service-worker.js')
-        .then(function(registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        })
-        .catch(function(err) {
-            console.log('ServiceWorker registration failed: ', err);
-        });
-}
