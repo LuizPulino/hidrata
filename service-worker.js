3ac0e32126d5a780d98abc1e8d683b545b89b2dd
Luiz Pulino <luizpulino@gmail.com>
2024-09-26 18:11:43 -0300

    .serviceworker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,42 @@
+const cachedName = "v1";
+
+// Step - Install Service Worker
 self.addEventListener("install", (e) => {
-    console.log("Service Worker Installed");
-  });
-  
-  self.addEventListener("activate", (e) => {
-    console.log("Service Worker Activated");
-  })
+  console.log("Service Worker Installed.");
+  // Step - Cache files
+});
+// Step - Activate Service Worker
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker Activated.");
+  //Step - Keep Cache light
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cachedName) {
+            console.log("Cached Service worker is being cleared.");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Step - Persisting cached assets
+self.addEventListener("fetch", (e) => {
+  console.log("fetching service worker.");
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const responseClone = res.clone();
+        caches.open(cachedName).then((cache) => {
+          cache.put(e.request, responseClone);
+        });
+        return res;
+      })
+      .catch((err) => {
+        caches.match(e.request).then((res) => res);
+      })
+  );
+});
